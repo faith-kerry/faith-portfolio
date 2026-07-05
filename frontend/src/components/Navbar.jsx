85a779bd0408b034fd44a0f2ac297bbd4c5c63eb
faith-kerry <kerubofaith944@gmail.com>
2026-07-05 11:31:56 +0300

    Create project structure and responsive navigation bar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-transparent">
+      <div className="container">
+
+        <a className="navbar-brand fw-bold fs-3" href="#">
+          Faith<span className="text-info">.</span>
+        </a>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+
+          <ul className="navbar-nav">
+
+            <li className="nav-item">
+              <a className="nav-link" href="#home">Home</a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#about">About</a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#skills">Skills</a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#projects">Projects</a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#blogs">Blogs</a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#contact">Contact</a>
+            </li>
+
+          </ul>
+
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
